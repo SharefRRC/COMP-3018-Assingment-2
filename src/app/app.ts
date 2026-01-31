ece147ace8b93b6ca52de879a95717d551b10cc7
SharefRRC <sislam48@academic.rrc.ca>
2026-01-31 16:11:37 -0600

    feat: add express app shell and global error handler

---
+++ src/app/app.ts
@@ -0,0 +1,10 @@
+import express from "express";
+import morgan from "morgan";
+import { errorHandler } from "./middleware/errorHandler";
+
+export const app = express();
+
+app.use(express.json());
+app.use(morgan("dev"));
+
+app.use(errorHandler);
